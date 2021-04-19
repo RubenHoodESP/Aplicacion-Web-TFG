4bbe229abfeaee69e79b3af089c0ed5631c0d366
RubenHoodESP <RubenHoodSpain@gmail.com>
2021-04-19 18:03:33 +0200

    Reading from csv

--- Analyzer.py
+++ Analyzer.py
@@ -5,14 +5,35 @@ from csv import reader
 with open('datos.csv', 'r') as csv_file:
     csv_reader = reader(csv_file)
     next(csv_reader)
-    filas = list(csv_reader)
-    print(filas)
+    tabla = list(csv_reader)
+    print("----- TABLA DE DATOS LEÍDA -----")
+    print(tabla)
 
+# Extraemos los lados exteriores
+lados_aux = [fila[1] for fila in tabla]
+lados_ext = [lado.split("-") for lado in lados_aux]
+print("----- LISTA DE LADOS EXTERIORES -----")
+print(lados_aux)
+print(lados_ext)
+
+# Extraemos la envoltura
+envoltura_aux = [fila[2] for fila in tabla]
+envoltura = [envoltura_aux.split("-") for envoltura_aux in envoltura_aux]
+print("----- LISTA DE ENVOLTURAS -----")
+print(envoltura_aux)
+print(envoltura)
+
+
+# Clasificación de locales
+def clasificador(tabla):
+    lados_aux = [fila[1] for fila in tabla]
+    lados_ext = [lado.split("-") for lado in lados_aux]
+    orden = [orden_locales for orden_locales in lados_ext if len(lados_ext) == 3]
+    return orden
+
+
+print(clasificador(tabla))
 # Creamos la tabla de datos inicial
-lados_ext = []
-envoltura = [2, 7]
-fila_tabla = [1, lados_ext, envoltura]
-tabla = [fila_tabla]
-print(tabla)
+
 
 # def analyzer(tabla):

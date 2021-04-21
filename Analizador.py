884bf4b2866e62d05d284072e3fbb43092f661c5
Ruben <rubenhoodspain@gmail.com>
2021-04-21 20:41:52 +0200

    Trazador A terminado

--- Analizador.py
+++ Analizador.py
@@ -1,7 +1,8 @@
 from csv import reader
 
 
-def lectura_csv():
+# Función que lee una tabla de datos de un fichero CSV
+def lectura_datos_csv():
     with open('datos.csv', 'r') as csv_file:
         csv_reader = reader(csv_file)
         next(csv_reader)
@@ -9,13 +10,24 @@ def lectura_csv():
         return tabla
 
 
-# Ordena lista con los locales A, B, D y E
-def ordenar():
+# Función que lee una tabla de datos de un fichero CSV
+def lectura_planta_csv():
+    with open('planta.csv', 'r') as csv_file:
+        csv_reader = reader(csv_file)
+        next(csv_reader)
+        tabla = list(csv_reader)
+        return tabla
+
+
+# Función que devuelve una lista con las posiciones de los locales
+# en orden. Primero A, B, D y finalmente E
+"""def ordenar():
     tabla = lectura_csv()
     n_local = 0
     orden_locales = []
     lados_aux = [fila[1] for fila in tabla]
     lados_ext = [lado.split("-") for lado in lados_aux]
+    print(lados_ext)
     for i in lados_ext:
         if len(i) == 3:
             orden_locales.append(n_local)
@@ -31,26 +43,54 @@ def ordenar():
             orden_locales.append(n_local)
         n_local += 1
     return orden_locales
+    """
 
 
-# Calcula el número de locales que tienen un mismo lado
-# exterior para cada uno de los lados exteriores existentes
-def n_locales():
-    tabla = lectura_csv()
-    num_locales = [0, 0, 0, 0]
+def clasificador():
+    tabla = lectura_datos_csv()
+    clasificacion = {}
     lados_aux = [fila[1] for fila in tabla]
     lados_ext = [lado.split("-") for lado in lados_aux]
-    print(lados_ext)
+    locales_A = [i for i in lados_ext if len(i) == 3]
+    locales_B = [i for i in lados_ext if len(i) == 2]
+    locales_D = [i for i in lados_ext if len(i) == 1 and "0" not in i]
+    locales_E = [i for i in lados_ext if len(i) == 1 and "0" in i]
+    clasificacion["A"] = locales_A
+    clasificacion["B"] = locales_B
+    clasificacion["D"] = locales_D
+    clasificacion["E"] = locales_E
+    return clasificacion
+
+
+# Función que calcula el número de locales que tienen un mismo lado
+# exterior para cada uno de los lados exteriores existentes
+def n_locales():
+    tabla = lectura_datos_csv()
+    num_locales = [0, 0, 0, 0]
+    locales = {}
+    lados_aux = [fila[1] for fila in tabla]
+    lados_ext = [lado.split("-") for lado in lados_aux]
     for i in lados_ext:
-        if "1" in i:
-            num_locales[0] += 1
-        elif "2" in i:
-            num_locales[1] += 1
-        elif "3" in i:
-            num_locales[2] += 1
-        elif "4" in i:
-            num_locales[3] += 1
-    return num_locales
+        for j in i:
+            if "1" in j:
+                num_locales[0] += 1
+            elif "2" in j:
+                num_locales[1] += 1
+            elif "3" in j:
+                num_locales[2] += 1
+            elif "4" in j:
+                num_locales[3] += 1
+    locales["N1"] = num_locales[0]
+    locales["N2"] = num_locales[1]
+    locales["N3"] = num_locales[2]
+    locales["N4"] = num_locales[3]
+    return locales
 
-
+"""
+print("LEEMOS TABLA DE DATOS:")
+print(lectura_datos_csv())
+print("\n Nº LOCALES POR LADO: N1, N2, N3 Y N4")
 print(n_locales())
+print("\n CLASIFICAMOS LOS DATOS:")
+print(clasificador())
+"""
--- Trazador.py
+++ Trazador.py
@@ -1,2 +1,60 @@
 import Analizador
 
+
+def trazar_A():
+    valores = Analizador.lectura_planta_csv()
+    aux = valores[0]
+    max_x = int(aux[0])
+    max_y = int(aux[1])
+    clasificacion = Analizador.clasificador()
+    locales = Analizador.n_locales()
+    if ["1", "2", "3"] in clasificacion["A"]:
+        v3x = v1x = v1y = 0.0
+        v4x = v2x = max_x
+        v2y = v1y
+        v4y = v3y = int(max_y) / max(int(locales["N1"]), int(locales["N3"]))
+        print("\n RESULTADOS 1,2,3")
+        print(v1x), print(v2x), print(v3x), print(v4x), print(v1y), print(v2y), print(v3y), print(v4y)
+
+    elif ["2", "3", "4"] in clasificacion["A"]:
+        v1x = v3x = max_x - (max_x / max(int(locales["N2"]), int(locales["N4"])))
+        v2x = v4x = max_x
+        v1y = v2y = 0.0
+        v3y = v4y = max_y
+        print("\n RESULTADOS 2,3,4")
+        print(v1x), print(v2x), print(v3x), print(v4x), print(v1y), print(v2y), print(v3y), print(v4y)
+
+    elif ["3", "4", "1"] in clasificacion["A"]:
+        v1x = v3x = 0.0
+        v2x = v4x = max_x
+        v3y = v4y = max_y
+        v2y = v1y = max_y - (max_y / max(int(locales["N1"]), int(locales["N3"])))
+        print("\n RESULTADOS 3,4,1")
+        print(v1x), print(v2x), print(v3x), print(v4x), print(v1y), print(v2y), print(v3y), print(v4y)
+
+    elif ["4", "1", "2"] in clasificacion["A"]:
+        v1x = v3x = 0.0
+        v2x = v4x = max_x / max(int(locales["N2"]), int(locales["N4"]))
+        v1y = v2y = 0.0
+        v3y = v4y = max_y
+        print("\n RESULTADOS 4,1,2")
+        print(v1x), print(v2x), print(v3x), print(v4x), print(v1y), print(v2y), print(v3y), print(v4y)
+
+
+def trazar_B():
+    valores = Analizador.lectura_planta_csv()
+    aux = valores[0]
+    max_x = int(aux[0])
+    max_y = int(aux[1])
+    clasificacion = Analizador.clasificador()
+    locales = Analizador.n_locales()
+    if ["1", "2"] in clasificacion["B"]:
+        v1x = v3x = 0.0
+        v1y = v2y = 0.0
+    elif ["2", "3"] in clasificacion["B"]:
+
+    elif ["3", "4"] in clasificacion["B"]:
+
+    elif ["4", "1"] in clasificacion["B"]:
+
+trazar_A()

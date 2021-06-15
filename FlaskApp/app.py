1b6bf0a88158a8df277c9c0708c9a32922944621
RubenHoodESP <RubenHoodSpain@gmail.com>
2021-06-15 18:41:38 +0200

    web

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/imagen_1")
+def imagen_1():
+    return render_template('imagen_1.html')
+
+@app.route("/imagen_2")
+def imagen_2():
+    return render_template('imagen_2.html')
+
+@app.route("/imagen_3")
+def imagen_3():
+    return render_template('imagen_3.html')
+
+@app.route("/imagen_4")
+def imagen_4():
+    return render_template('imagen_4.html')
+
+@app.route("/imagen_5")
+def imagen_5():
+    return render_template('imagen_5.html')
+
 
 if __name__ == "__main__":
     app.run()

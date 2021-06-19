302b36f3ad1058b1c8d59089a572c3e186896b3d
RubenHoodESP <RubenHoodSpain@gmail.com>
2021-06-19 16:37:03 +0200

    hotfix app web

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 UPLOAD_FOLDER = '../'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -12,26 +13,20 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods = ['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+
+@app.route("/")
+def index():
     return render_template('index.html')
 
-@app.route("/imagen_1")
+
+@app.route("/upload", methods = ['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    return "File saved successfully"
+
+@app.route("/imagen_1", methods = ['POST', 'GET'])
 def imagen_1():
     return render_template('imagen_1.html')
 
@@ -53,4 +48,4 @@ def imagen_5():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', port=5000)
